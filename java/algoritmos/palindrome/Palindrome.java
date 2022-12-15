75ebf603b96ee1c59f6538f6c1e411b23e6acbb0
Jorge Quintero <jorgequintero142@gmail.com>
2022-12-15 16:11:36 -0500

    Program tells you if a sentence is or not  a palindrome

---
+++ java/algoritmos/palindrome/Palindrome.java
@@ -0,0 +1,21 @@
+public class Palindrome {
+
+	public static void main(String[] jorge) {
+
+		String originalText = "dabale arroz a la Zorra el abad";
+		// remove spaces and continue using the same variable
+		String result =  String.format("Is '%s' a palindrome? ", originalText);
+		originalText = originalText.replace(" ", "");
+		String reverse = reverseString(originalText);
+		System.out.println(result + originalText.equalsIgnoreCase(reverse));
+
+	}
+
+	static String reverseString(String txt) {
+		String reverse = "";
+		for (int i = txt.length() - 1; i >= 0; i--) {
+			reverse += txt.charAt(i);
+		}
+		return reverse;
+	}
+}
