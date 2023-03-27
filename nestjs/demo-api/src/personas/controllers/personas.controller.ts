9be20512816f6be04e4436628a45deb1883f9f22
Jorge Quintero <jorgequintero142@gmail.com>
2023-03-26 20:32:54 -0500

    add decorator to personas controller for explain http responses code

--- nestjs/demo-api/src/personas/controllers/personas.controller.ts
+++ nestjs/demo-api/src/personas/controllers/personas.controller.ts
@@ -1,8 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
-import { ApiOperation } from '@nestjs/swagger/dist';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger/dist';
 import { PersonaDto } from '../dtos/persona.dto';
-import { Persona } from '../entities/persona.entity';
 import { PersonasService } from '../services/personas.service';
 
 @ApiTags('Personas')
@@ -11,12 +10,20 @@ export class PersonasController {
   constructor(private personasService: PersonasService) {}
 
   @ApiOperation({ description: 'Listar todas las personas' })
+  @ApiResponse({
+    status: 200,
+    description: 'Se han listado todas las personas ',
+  })
   @Get()
   findAll() {
     return { personas: this.personasService.findAll() };
   }
 
   @ApiOperation({ description: 'Crear nueva persona' })
+  @ApiResponse({
+    status: 201,
+    description: 'Se ha registrado una nueva persona',
+  })
   @Post()
   create(@Body() payload: PersonaDto) {
     return { persona: this.personasService.create(payload) };
